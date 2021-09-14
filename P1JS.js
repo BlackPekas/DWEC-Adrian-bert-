7e4db8aa9db183616e31e3d018170f8e563180a7
Adrian Bertó <73901763+BlackPekas@users.noreply.github.com>
2021-09-14 17:21:56 +0200

    Prueba 1 Adrián Bertó 14/09/2021

---
+++ P1JS.js
@@ -0,0 +1,13 @@
+
+function funcion1() {
+    document.getElementById("t1").innerHTML = "Hola ^^";
+}
+function funcion2() {
+    document.write("Bon Dia.");
+}
+function funcion3() {
+    window.alert("F En el Chat.");
+}
+function funcion4() {
+    console.log("Hola Consola");
+}
